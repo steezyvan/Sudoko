01a344397e8831066d2883fdc9996af29fb9450d
steezyvan <91402817+steezyvan@users.noreply.github.com>
2024-04-22 18:44:25 -0700

    Third Commit

--- src/sudoku/Coordinates.java
+++ src/sudoku/Coordinates.java
@@ -1,2 +1,21 @@
-package sudoku;public class Coordinates {
+package sudoku;
+
+public class Coordinates {
+
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+//5:27 https://www.youtube.com/watch?v=qH9mWpYMtYU&t=34s
 }
--- src/sudoku/SudokuGame.java
+++ src/sudoku/SudokuGame.java
@@ -1,4 +1,24 @@
-package sample;
+package sudoku;
 
-public class Controller {
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return Sudoku.Utilities.copyToNewArray(gridState);
+    }
 }
