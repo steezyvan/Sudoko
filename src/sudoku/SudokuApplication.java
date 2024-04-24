116050d3a79eacb7f0598edaa52ccb17d9bcc50a
steezyvan <91402817+steezyvan@users.noreply.github.com>
2024-04-23 20:11:49 -0700

    Sixth Commit

--- src/sudoku/SudokuApplication.java
+++ src/sudoku/SudokuApplication.java
@@ -1,2 +1,27 @@
-package sudoku;public class SudokuApplication {
+package sudoku;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scence.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        uiImple = UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
