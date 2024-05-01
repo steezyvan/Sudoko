33feaf71eef0d08f709b6a7dc4410015d88e56c6
steezyvan <91402817+steezyvan@users.noreply.github.com>
2024-04-30 21:00:40 -0700

    Thirteenth Commit

--- src/sudoku/computationlogic/GameGenerator.java
+++ src/sudoku/computationlogic/GameGenerator.java
@@ -11,6 +11,19 @@ public class GameGenerator {
     public static int[][] getNewGameGrid() {
         return unsolveGame(getSolvedGame));
     }
+
+    private static int[][] unsolveGame(int[][] solvedGame) {
+        Random random = new Random(System.currentTimeMillis());
+
+        boolean solvable = false;
+        int solveableArray = new int[GRID_BOUNDARY][GRID_BOUNDARY];
+        while
+
+
+        return new int[0][];
+    }
+
+
     private static int [][] getSolvedGame()) {
        Random random = new Random(System.currentTimeMillis());
        int[][] newGrid = new int[GRID_BOUNDARY][GRID_BOUNDARY];
@@ -29,8 +42,34 @@ public class GameGenerator {
                        newGrid[coord.getX()][coord.getY()] = 0;
                    });
 
+                   interrupt = 0;
+                   allocations = 0;
+                   allocTracker.clear();
+                   attempts++ ;
+
+                   if(attempts > 500) {
+                       clearArray(newGrid);
+                       attempts = 0;
+                       value = 1;
+                   }
+               }
+
+               int xCoordinate = random.nextInt(GRID_BOUNDARY);
+               int yCoordinate = random.nextInt(GRID_BOUNDARY);
+
+               if (newGrid[xCoordinate][yCoordinate] == 0) {
+                    newGride[xCoordinate][yCoordinate] = value;
+
+                    if(GameLogic.sudokuIsInvalid(newGrid)){
+                        newGrid[xCoordinate][yCoordinate] = 0;
+                        interrupt++;
+                    } else {
+                        allocTracker.add(new Coordinates(xCoordinate, yCoordinate));
+                        allocations++;
+                    }
                }
            }
        }
+       return newGrid;
     }
 }
